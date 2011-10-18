e30ac74c7d70299bf8a9f4e15f57554a2bf58c2f
Steven Armstrong <steven@icarus.ethz.ch>
2011-10-18 19:40:41 +0200

    implement fetching a tarball from tftp
    Signed-off-by: Steven Armstrong <steven@icarus.ethz.ch>

--- conf/type/__stage/files/lib.sh
+++ conf/type/__stage/files/lib.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+output_if_debug() {
+   if [ "$__debug" = "yes" ]; then
+      cat
+   fi
+}
 
+# download a stage tarball from tftp
+fetch_tftp() {
+   local uri="$1"
+   local target="$2"
 
+   uri=$(echo "${uri}" | sed -e 's|^tftp://||')
+   host=$(echo "${uri}" | cut -d / -f 1)
+   path=$(echo "${uri}" | cut -d / -f 2-)
+   tftp -g -r "${path}" -l "${target}" "${host}" || die "could not fetch ${uri}"
+}
